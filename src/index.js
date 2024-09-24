82a5b5f884001bca0bc7074e808879459f8f68f4
ElectricCrimson <synthetic0aesthetic@gmail.com>
2024-09-24 19:51:02 +0300

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    const num = `${Math.abs(n)}`;
+    let result = '';
+    let i = num.length - 1;
+
+    while(i >= 0) {
+        result += num[i];
+        i -= 1;
+    }
+    
+    return -(-result);
 }
